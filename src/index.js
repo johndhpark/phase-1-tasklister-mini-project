632395749ff18f62e7b993e686b70522c7ca146d
John DH. Park <john.dh.park@me.com>
2023-04-09 11:28:12 -0700

    completed tasklist-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const input = form.elements["new-task-description"];
+  const todos = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const task = input.value;
+    const newTodo = document.createElement("li");
+    newTodo.textContent = task;
+
+    todos.append(newTodo);
+
+    input.value = "";
+  });
 });
